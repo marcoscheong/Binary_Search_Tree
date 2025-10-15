807852a2746f45bab1b737e4338398e98ff81b56
marcoscheong <cheong.hao.ting.marcos_2025@nyjc.edu.sg>
2025-10-15 06:35:43 +0000

    update

--- bst.py
+++ bst.py
@@ -2,25 +2,60 @@ class Node:
     """
     """
     def __init__(self, key):
-        pass
+        self.left = None
+        self.right = None
+        self.key = key
   
 class BinarySearchTree:
     """
     """
     def __init__(self):
-        pass
+        self.root = None
   
     def is_empty(self):
         """
         """
-        pass
+        return self.root is None
     
     def insert(self, key):
         """
         """
-        pass
+        node = Node(key)
+        if self.is_empty():
+            self.root = node
+        else:
+            current = self.root
+            parent = None
+
+            while current:
+                parent = current
+
+                if key < current.key:
+                    current = current.left
+                    
+                elif key > current.key:
+                    current = current.right
+            
+            if key < parent.key:
+                parent.left = node
+
+            elif key >= parent.key:
+                parent.right = node
+                
+              
     
     def search(self, key):
         """
         """
-        pass
+        current = self.root
+        
+        while current:
+            if key == current.key:
+                return True
+            elif key < current.key:
+                current = current.left
+            elif key > current.key:
+                current = current.right
+            
+        return False
+            
